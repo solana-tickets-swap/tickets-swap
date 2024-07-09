bb2d06695a7a6967ea40b4fb3c24a18998777c8e
cecile hirschauer <cecile.h.75@gmail.com>
2024-07-09 12:13:37 +0200

    Creation react project à la racine de app/

--- app/tickets-swap/src/pages/CreateEvent.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CreateEvent() {
-    return <div>Page CreateEvent...</div>;
-}
-
-export default CreateEvent;
--- app/tickets-swap/src/pages/SwapTicket.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CreateEvent() {
-    return <div>Page SwapTicket...</div>;
-}
-
-export default CreateEvent;
