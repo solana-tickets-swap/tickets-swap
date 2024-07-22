e0d522b66abe5211642f90ae4c86ec370113fbff
s-damian <contact@damian-freelance.fr>
2024-07-22 18:32:14 +0300

    Optimize React

--- app/tickets-swap/src/components/NavBar.tsx
+++ app/tickets-swap/src/components/NavBar.tsx
@@ -17,22 +17,22 @@ const NavBar: React.FC = () => {
             <div className="container mx-auto flex justify-between items-center">
                 <ul className="hidden md:flex justify-around items-center list-none p-0 m-0 w-full">
                     <li>
-                        <Link href="/" className="text-white no-underline p-2 hover:border-b-2 hover:border-gray-300">
+                        <Link href="/" className="text-white no-underline p-2 hover:text-yellow-300">
                             Accueil
                         </Link>
                     </li>
                     <li>
-                        <Link href="/create-event" className="text-white no-underline p-2 hover:border-b-2 hover:border-gray-300">
+                        <Link href="/create-event" className="text-white no-underline p-2 hover:text-yellow-300">
                             Créer un Événement
                         </Link>
                     </li>
                     <li>
-                        <Link href="/list-events" className="text-white no-underline p-2 hover:border-b-2 hover:border-gray-300">
+                        <Link href="/list-events" className="text-white no-underline p-2 hover:text-yellow-300">
                             Liste des Événements
                         </Link>
                     </li>
                     <li>
-                        <Link href="/verify-nft" className="text-white no-underline p-2 hover:border-b-2 hover:border-gray-300">
+                        <Link href="/verify-nft" className="text-white no-underline p-2 hover:text-yellow-300">
                             Vérifier un NFT
                         </Link>
                     </li>
@@ -49,22 +49,22 @@ const NavBar: React.FC = () => {
             {isOpen && (
                 <ul className="md:hidden flex flex-col items-center list-none p-0 m-0 bg-blue-600">
                     <li className="w-full text-center">
-                        <Link href="/" className="block text-white no-underline p-2 hover:border-b-2 hover:border-gray-300" onClick={toggleMenu}>
+                        <Link href="/" className="block text-white no-underline p-2 hover:text-yellow-300" onClick={toggleMenu}>
                             Accueil
                         </Link>
                     </li>
                     <li className="w-full text-center">
-                        <Link href="/create-event" className="block text-white no-underline p-2 hover:border-b-2 hover:border-gray-300" onClick={toggleMenu}>
+                        <Link href="/create-event" className="block text-white no-underline p-2 hover:text-yellow-300" onClick={toggleMenu}>
                             Créer un Événement
                         </Link>
                     </li>
                     <li className="w-full text-center">
-                        <Link href="/list-events" className="block text-white no-underline p-2 hover:border-b-2 hover:border-gray-300" onClick={toggleMenu}>
+                        <Link href="/list-events" className="block text-white no-underline p-2 hover:text-yellow-300" onClick={toggleMenu}>
                             Liste des Événements
                         </Link>
                     </li>
                     <li className="w-full text-center">
-                        <Link href="/verify-nft" className="block text-white no-underline p-2 hover:border-b-2 hover:border-gray-300" onClick={toggleMenu}>
+                        <Link href="/verify-nft" className="block text-white no-underline p-2 hover:text-yellow-300" onClick={toggleMenu}>
                             Vérifier un NFT
                         </Link>
                     </li>
